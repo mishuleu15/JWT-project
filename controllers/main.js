b1d2c1774e4276b6266f61123467a0d9c6144e6d
unknown <mishu_leu15@yahoo.com>
2021-12-01 21:18:29 +0200

    first commit/ new project/ created basic server, controllers, routes, middlewares

---
+++ controllers/main.js
@@ -0,0 +1,19 @@
+const CustomAPIError = require('../errors/custom-error');
+
+const login = async (req, res) => {
+  const { username, password } = req.body;
+  if (!username || !password) {
+    throw new CustomAPIError('Please provide email and password', 400);
+  }
+  res.send('Fake Login/Register/Signup');
+};
+
+const dashBoard = async (req, res) => {
+  const luckyNumber = Math.floor(Math.random() * 100);
+  res.status(200).json({
+    msg: `Hello, John Doe`,
+    secret: `Here is the data, the number is ${luckyNumber}`,
+  });
+};
+
+module.exports = { login, dashBoard };
