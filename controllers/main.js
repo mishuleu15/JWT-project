5b9d7af607c704fe2659d21c752306b483d170e6
unknown <mishu_leu15@yahoo.com>
2021-12-02 21:18:25 +0200

    added jwt and auth middleware, refactored custom errors, finished project

--- controllers/main.js
+++ controllers/main.js
@@ -1,17 +1,34 @@
-const CustomAPIError = require('../errors/custom-error');
+const { BadRequestError } = require('../errors');
+
+const jwt = require('jsonwebtoken');
 
 const login = async (req, res) => {
   const { username, password } = req.body;
+  // mongoose validation
+  // Joi
+  // check in the controller
+
   if (!username || !password) {
-    throw new CustomAPIError('Please provide email and password', 400);
+    throw new BadRequestError('Please provide email and password');
   }
-  res.send('Fake Login/Register/Signup');
+
+  //just for demo, normally provided by DB!!!!
+  const id = new Date().getDate();
+
+  // try to keep payload small, better experience for user
+  // just for demo, in production use long, complex and unguessable string value!!!!!!!!!
+  const token = jwt.sign({ id, username }, process.env.JWT_SECRET, {
+    expiresIn: '30d',
+  });
+
+  res.status(200).json({ msg: 'user created', token });
 };
 
 const dashBoard = async (req, res) => {
   const luckyNumber = Math.floor(Math.random() * 100);
+
   res.status(200).json({
-    msg: `Hello, John Doe`,
+    msg: `Hello, ${req.user.username}`,
     secret: `Here is the data, the number is ${luckyNumber}`,
   });
 };
--- routes/main.js
+++ routes/main.js
@@ -4,7 +4,9 @@ const router = express.Router();
 
 const { login, dashBoard } = require('../controllers/main');
 
-router.route('/dashboard').get(dashBoard);
+const authMiddleware = require('../middleware/auth');
+
+router.route('/dashboard').get(authMiddleware, dashBoard);
 router.route('/login').post(login);
 
 module.exports = router;
